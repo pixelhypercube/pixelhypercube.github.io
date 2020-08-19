fc8761ff0d271d0e5cef84c27d24050d2574e875
pixelhypercube <6thetce@gmail.com>
2020-08-19 14:39:30 +0800

    Reinitialised repo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# pixelhypercube.github.io
-The repo for my website!
+# Hi everyone! Welcome to my website repo!
+
+This repo stores all of my codes to render my personal website - [pixelhypercube.github.io](pixelhypercube.github.io) .
+
+This website is a portfolio about who I am, what I like to do and my coding projects (more of a resume).
+
+Feel free to breeze through my website!
+
+Thanks a lot for stopping by!
---
+++ public/js/programming.js
@@ -0,0 +1,52 @@
+var url = `http://localhost:3000`;
+const DEBUG = true;
+if (!DEBUG) {
+    url = `http://phcWebsite.herokuapp.com`;
+}
+
+function getDevelopment(dev) {
+    if (dev==0) {
+        return "";
+    } else if (dev==1) {
+        return "(In development)";
+    } else if (dev==2) {
+        return "(Alpha Phase)";
+    } else if (dev==3) {
+        return "(Beta Phase)";
+    }
+}
+
+function genCodingProjects() {
+    $.get(`${url}/codingprojects`)
+    .done((res)=>{
+        for (let i in res) {
+            var insertHTML = `
+                <div class="card m-3">
+                            <div class="card-header">
+                                <h3>${res[i].title}</h3>
+                            </div>
+                            <div class="card-body">
+                                <img style="width:250px;" class="coding-logo" src="${res[i].image_location}" alt="proj${res[i].image_location}" >
+                                <hr>
+                                <div class="container">
+                                    <p>${getDevelopment(res[i].in_development)}</p>
+                                    <p>${res[i].brief_description}</p>
+                                    <a class="btn m-1" style="background-color: #185050;color:white;border-radius:0" class="sectionBtn" href="./codingProject.html?proj_id=${res[i].proj_id}">View Project</a>
+                                    <a class="btn m-1" style="background-color: #183250;color:white;border-radius:0" class="sectionBtn" href="${res[i].link}">Go to project!</a>
+                                </div>
+                            </div>
+                        </div>
+            `;
+            $("#coding_projects").append(insertHTML);
+            $("div.card-body").each(function(i){
+                $(this).css({"background":`rgb(130, ${185+Math.floor(Math.random()*5)}, ${240+Math.floor(Math.random()*20)})`})
+            });
+        }
+    })
+    .fail((err)=>{
+
+    });
+}
+$(document).ready(function(){
+    genCodingProjects();
+});
